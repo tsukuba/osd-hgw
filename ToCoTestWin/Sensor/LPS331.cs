3787ac719e1c9e319ffcee5eb494bf62c5fdc38f
genyakun <gennya@gmail.com>
2015-07-16 19:19:07 +0900

    Implement for LPS331 Sensor data parse

--- ToCoTestWin/Sensor/LPS331.cs
+++ ToCoTestWin/Sensor/LPS331.cs
@@ -8,21 +8,39 @@ namespace ToCoTestWin.Sensor
 {
     class LPS331 : _Sensor
     {
-        int Temp, Temp2;
-        uint Humidity, Pressure;
+        ushort Temp;
+        uint Pressure;
 
         public LPS331(byte[] data) : base(data)
         {
             SensorID = 0x02;
-            Temp = BitConverter.ToInt32(data, 0);
-            Humidity = BitConverter.ToUInt32(data, 4);
-            Pressure = BitConverter.ToUInt32(data, 8);
+            Pressure = BitConverter.ToUInt32(rawdata, 0);
+            Temp = BitConverter.ToUInt16(rawdata, 5);
         }
 
         public void ShowData()
         {
-            Console.WriteLine("Temp: {0}, Hum: {1}, Pre: {2}", Temp / 100.0f, Humidity / 1024.0f, Pressure / 100.0f);
-            Console.WriteLine("Temp: {0}, Hum: {1}, Pre: {2}", Temp, Humidity, Pressure);
+            double press, temp;
+            if ((Pressure & (1 << 23)) != 0)
+            {
+                 press = -((Pressure ^ 0xFFFFFF) + 1) / 4096.0f;
+            }
+            else
+            {
+                press = Pressure / 4096.0f;
+            }
+
+            if ((Temp & (1 << 15)) != 0)
+            {
+                temp = 42.5 - ((Temp ^ 0xFFFF) + 1) / 480.0f;
+            }
+            else
+            {
+                temp = 42.5 + Temp / 480.0f;
+            }
+            
+            Console.WriteLine("Temp: {0}, Hum: {1}, Pre: {2}", temp, press);
+            Console.WriteLine("Temp: {0}, Hum: {1}, Pre: {2}", Temp, Pressure);
         }
     }
 }
